f596a13a6fe6d937d6a0aa9c748da1a6a0ceb2d5
shimatachi <thoriqalbarp1@gmail.com>
2023-01-03 01:36:42 +0700

    Implement API 3

---
+++ app/src/main/java/com/example/myapplication/ui/absensi/Matakuliah.java
@@ -0,0 +1,28 @@
+package com.example.myapplication.ui.absensi;
+
+public class Matakuliah {
+    private String namamatkul;
+//    private String totalabsensi;
+
+    public Matakuliah(String namamatkul) {
+        this.namamatkul = namamatkul;
+//        this.totalabsensi = totalabsensi;
+    }
+
+    public String getNamamatkul() {
+        return namamatkul;
+    }
+
+//    public void setNamamatkul(String namamatkul) {
+//        this.namamatkul = namamatkul;
+//    }
+
+//    public String getTotalabsensi() {
+//        return totalabsensi;
+//    }
+
+//    public void setTotalabsensi(String totalabsensi) {
+//        this.totalabsensi = totalabsensi;
+//    }
+}
+
